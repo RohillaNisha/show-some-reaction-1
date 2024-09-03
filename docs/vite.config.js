13e67fa8e006d982e374ee27ba4bfe1a9608a348
Nisha <nisha.bioinfo@gmail.com>
2024-09-03 14:42:07 +0200

    added build command and changed vite config

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // or 'dist' if that’s what you use
+  },
   server: {
     proxy: {
       "/rest": "http://localhost:3333",
